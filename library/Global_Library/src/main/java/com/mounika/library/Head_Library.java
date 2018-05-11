4acfce102e7a011d8140eabb63ec9448105e4d94
mounikabommakanti <mounika10.bommakanti@gmail.com>
2018-05-11 02:48:42 -0500

    formats done to library interface

--- library/Global_Library/src/main/java/com/mounika/library/Head_Library.java
+++ library/Global_Library/src/main/java/com/mounika/library/Head_Library.java
@@ -2,9 +2,13 @@ package com.mounika.library;
 
 public interface Head_Library {
 	String getBookTitle();
+
 	String getAuthor();
+
 	String getEdition();
+
 	String getGenre();
+
 	String getAvailability();
 
 }
