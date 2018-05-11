0237292106474fb6ec1e54a2e65ffa2a2694e6ec
mounikabommakanti <mounika10.bommakanti@gmail.com>
2018-05-11 02:31:43 -0500

    formatted java collections and super()

--- JavaCollections/src/com/mounika/collections/ArrayListDemo.java
+++ JavaCollections/src/com/mounika/collections/ArrayListDemo.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class ArrayListDemo {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ArrayList<Integer> arrayListInt = new ArrayList<Integer>();
 		int sum = 0;
 		arrayListInt.add(1);
--- JavaCollections/src/com/mounika/collections/LinkedListDemo.java
+++ JavaCollections/src/com/mounika/collections/LinkedListDemo.java
@@ -6,26 +6,22 @@ import java.util.LinkedList;
 public class LinkedListDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedList<Integer> linkedlist=new LinkedList<Integer>();
+		LinkedList<Integer> linkedlist = new LinkedList<Integer>();
 		linkedlist.add(1);
 		linkedlist.add(2);
 		linkedlist.add(3);
-		Iterator<Integer> itr1=linkedlist.iterator();
+		Iterator<Integer> itr1 = linkedlist.iterator();
 		System.out.println("initial set of elements in linkedlist: ");
-		while(itr1.hasNext())
+		while (itr1.hasNext())
 			System.out.println(itr1.next());
 		linkedlist.removeFirst();
 		linkedlist.addFirst(3);
 		linkedlist.removeLast();
 		linkedlist.addLast(1);
-		Iterator<Integer> itr2=linkedlist.iterator();
+		Iterator<Integer> itr2 = linkedlist.iterator();
 		System.out.println("Second set of elements in linkedlist after adding and removing elements: ");
-		while(itr2.hasNext())
+		while (itr2.hasNext())
 			System.out.println(itr2.next());
-		
-		
-
 	}
 
 }
