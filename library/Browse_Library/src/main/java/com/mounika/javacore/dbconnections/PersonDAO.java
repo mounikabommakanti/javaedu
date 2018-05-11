3ac49193aa8a5c88e849177098da5ed07c6c1263
mounikabommakanti <mounika10.bommakanti@gmail.com>
2018-05-11 02:42:53 -0500

    formatted sql connection singleton class

--- library/Browse_Library/src/main/java/com/mounika/javacore/dbconnections/PersonDAO.java
+++ library/Browse_Library/src/main/java/com/mounika/javacore/dbconnections/PersonDAO.java
@@ -11,7 +11,7 @@ public class PersonDAO {
 	public void insert(int id, String name, int age) throws SQLException {
 		Connection con = SqlConnection.getConnection();
 		String insertTableSQL = "INSERT INTO Person" + "(id,name,age) VALUES" + "(?,?,?)";
-		PreparedStatement ps= con.prepareStatement(insertTableSQL);
+		PreparedStatement ps = con.prepareStatement(insertTableSQL);
 		ps.setInt(1, id);
 		ps.setString(2, name);
 
@@ -19,19 +19,20 @@ public class PersonDAO {
 		ps.executeUpdate();
 		System.out.println("row inserted");
 	}
-	public void getPerson(int id ) throws SQLException {
+
+	public void getPerson(int id) throws SQLException {
 		Connection con = SqlConnection.getConnection();
 		String selectTableSQL = "SELECT *FROM Person WHERE id=?";
-		PreparedStatement ps= con.prepareStatement(selectTableSQL);
+		PreparedStatement ps = con.prepareStatement(selectTableSQL);
 		ps.setInt(1, id);
-		ResultSet rs= ps.executeQuery();
-		while(rs.next()) {
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
 			System.out.println(rs.getInt("id"));
 			System.out.println(rs.getString("name"));
 			System.out.println(rs.getInt("age"));
 		}
-		
-		//sSystem.out.println("row in");
-		
+
+		// sSystem.out.println("row in");
+
 	}
 }
--- library/Browse_Library/src/main/java/com/mounika/library/Browse_Books.java
+++ library/Browse_Library/src/main/java/com/mounika/library/Browse_Books.java
@@ -2,12 +2,12 @@ package com.mounika.library;
 
 import com.mounika.Texas_Inventory.Head_Library_Impl;
 
-public class Browse_Books  {
+public class Browse_Books {
 	public static void main(String args[]) {
-		Head_Library browse_books=new Head_Library_Impl();
+		Head_Library browse_books = new Head_Library_Impl();
 		System.out.println(browse_books.getGenre());
 		System.out.println(browse_books.getBookTitle());
-		System.out.println(browse_books.getAuthor());		
+		System.out.println(browse_books.getAuthor());
 		System.out.println(browse_books.getEdition());
 		System.out.println(browse_books.getAvailability());
 	}
