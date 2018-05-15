c6fc75c76c6c67ac418f28e6ce1f5cfb67c57416
Mouni Bmk <mounika10.bommakanti@gmail.com>
2018-05-15 02:37:15 -0500

    Update Shape.java

--- corejavaconcepts/src/com/mounika/abstractclass/Shape.java
+++ corejavaconcepts/src/com/mounika/abstractclass/Shape.java
@@ -1,7 +1,16 @@
 package com.mounika.abstractclass;
 
 public abstract class Shape {
+	String shapename;
+
 	public abstract int area();
 
 	public abstract int perimeter();
+
+	public String shapeName(String shapename)
+
+	{
+		this.shapename = shapename;
+		return shapename;
+	}
 }
